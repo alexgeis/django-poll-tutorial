0c4a83ed49a01dce380b85b1fac2c401c55374dd
Alex Geis <siegxela@gmail.com>
2023-03-12 14:16:31 -0600

    add views for index.html and detail.html, passsing into each request the relevant context

--- polltut/polls/views.py
+++ polltut/polls/views.py
@@ -7,7 +7,9 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    context = {
+        'latest_question_list': latest_question_list
+    }
     return render(request, 'polls/index.html', context)
 
 
@@ -16,7 +18,10 @@ def detail(request, question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
