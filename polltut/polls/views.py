1e3a8c79550d2f1ac48af0b7c92349ba046cf764
Alex Geis <siegxela@gmail.com>
2023-03-12 19:47:29 -0600

    update IndexView() generic view method get_queryset to validate against showing a future dated question

--- polltut/polls/views.py
+++ polltut/polls/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.db.models import F
 
 # Create your views here.
@@ -58,8 +59,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        # """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+
+        Question.objects.filter(pub_date__lte=timezone.now()) returns a queryset containing Questions whose pub_date is less than or equal to - that is, earlier than or equal to - timezone.now.
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
